d43ec46818d90308edd616f038ac2c34f9c1bb58
Arne Hassel <arne.hassel@capgemini.com>
2023-10-06 22:38:15 +0200

    Removing vocabularies, to easen the code exported

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export {typeIndexContext} from "@/ldo/typeIndex.context";
 export {typeIndexSchema} from "@/ldo/typeIndex.schema";
 export {TypeIndexShapeType, TypeRegistrationShapeType} from "@/ldo/typeIndex.shapeTypes";
 export type {TypeIndex, TypeRegistration} from "@/ldo/typeIndex.typings";
-export {solid} from "@/vocabularies";
